b7b8d0f96329f1cbc5967ca432fdc07552b89af9
Dhikigame <dadada7772000@yahoo.co.jp>
2015-06-29 02:56:48 +0900

    Update func.h

--- 宝探しゲーム/宝探し/func.h
+++ 宝探しゲーム/宝探し/func.h
@@ -1,13 +1,14 @@
 //起動後初期化
 GLOBAL void ini();
 GLOBAL void load();
-//Menu
+//switchでメニュー切り替え
 GLOBAL void Menu();
 GLOBAL void Easy();
 GLOBAL void Normal();
 GLOBAL void Hard();
 GLOBAL void Result();
 GLOBAL void Help();
-//func
+//キー入力受け付け（キー入力いらっしゃい））
 GLOBAL int gpUpdateKey();
+//ランダムに箱セット
 GLOBAL void Boxset();
